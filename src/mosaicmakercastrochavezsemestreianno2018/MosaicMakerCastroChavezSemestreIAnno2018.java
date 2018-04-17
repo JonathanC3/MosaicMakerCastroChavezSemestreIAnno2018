3177279ca345c71985f1eefb5ae64bec62953934
JonathanC3 <37557466+JonathanC3@users.noreply.github.com>
2018-04-16 20:09:23 -0600

    proyecto 2

---
+++ src/mosaicmakercastrochavezsemestreianno2018/MosaicMakerCastroChavezSemestreIAnno2018.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mosaicmakercastrochavezsemestreianno2018;
+
+import GUI.MainWindow;
+import javafx.application.Application;
+
+
+/**
+ *
+ * @author jonathan
+ */
+public class MosaicMakerCastroChavezSemestreIAnno2018 {
+    
+    
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Application.launch(MainWindow.class, args);
+    }
+    
+}
