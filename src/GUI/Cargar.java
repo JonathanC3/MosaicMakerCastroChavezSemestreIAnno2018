6a9171aab45d24f5bd2e3845f409b09ba77c8986
JonathanC3 <jonathan@jonathan-Satellite-C45-A>
2018-04-22 11:55:08 -0600

    Scrollbar en imagen

--- src/GUI/Cargar.java
+++ src/GUI/Cargar.java
@@ -11,13 +11,22 @@ import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.Priority;
 import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 import javafx.stage.Stage;
@@ -29,19 +38,38 @@ import javax.imageio.ImageIO;
 public class Cargar extends Application {
     
     ImageView myImageView;
+    ScrollPane sp;
+    Image images;
+//    VBox vb;
+//    Pane pane;
+//    Label fileName;
+//    String imageName;
+//    Canvas cv;
     
     @Override
     public void start(Stage primaryStage) {
+       
+        
         
         Button btnLoad = new Button("Load");
         btnLoad.setOnAction(btnLoadEventListener);
         
-        myImageView = new ImageView();        
+
+        myImageView = new ImageView();
         
+        sp = new ScrollPane();
+        sp.setPrefSize(300, 250);
+        sp.setVbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
+        sp.setHbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
+        sp.setContent(myImageView);
+
+        //VBox rootBox = new VBox();
+        //rootBox.getChildren().addAll(btnLoad, scrollPane);
         VBox rootBox = new VBox();
-        rootBox.getChildren().addAll(btnLoad, myImageView);
+        rootBox.getChildren().addAll(btnLoad, sp);
         
-        Scene scene = new Scene(rootBox, 300, 300);
+        
+        Scene scene = new Scene(rootBox, 400, 400);
         
         primaryStage.setTitle("cargar");
         primaryStage.setScene(scene);
@@ -56,7 +84,12 @@ public class Cargar extends Application {
 
         @Override
         public void handle(ActionEvent t) {
-            FileChooser fileChooser = new FileChooser();
+    
+            getImageView();
+        }
+    };
+    private ImageView getImageView(){
+        FileChooser fileChooser = new FileChooser();
             
             //Set extension filter
             FileChooser.ExtensionFilter extFilterJPG = new FileChooser.ExtensionFilter("JPG files (*.jpg)", "*.jpg");
@@ -73,12 +106,17 @@ public class Cargar extends Application {
                 Image image = SwingFXUtils.toFXImage(bufferedImage, null);
                 
                 myImageView.setImage(image);
+                sp.setContent(null);
+                sp.setContent(myImageView);
                 
                 
             } catch (IOException ex) {
                 Logger.getLogger(MainWindow.class.getName()).log(Level.SEVERE, null, ex);
             }
-
-        }
-    };
+            return myImageView;
+    }
+    private ScrollPane getScrollPane(){
+        
+    return sp;
+    }
 }
