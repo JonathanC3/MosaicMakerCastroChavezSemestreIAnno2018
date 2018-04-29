8e1d2bae8959fbb1664d0d31bac6fe7c7f3bab31
JonathanC3 <jonathan@jonathan-Satellite-C45-A>
2018-04-28 22:12:06 -0600

    listo

---
+++ src/data/ProjectData.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package data;
+import domain.ImageObject;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author root
+ */
+public class ProjectData {
+    private String path;
+    
+    public ProjectData() {
+        this.path = "Proyects.dat";
+    }
+    
+    public void saveProyect(ImageObject project) throws IOException, ClassNotFoundException{
+        File file= new File(this.path);
+        List<ImageObject> objetoList= new ArrayList<ImageObject>();
+        
+        if(file.exists()){
+            ObjectInputStream objectInputStream= new ObjectInputStream(new FileInputStream(file));
+            Object aux= objectInputStream.readObject();
+            objetoList=(List<ImageObject>) aux;
+            objectInputStream.close();
+        }
+        
+        objetoList.add(project);
+        ObjectOutputStream output= new ObjectOutputStream(new FileOutputStream(file));
+        output.writeUnshared(objetoList);
+        output.close();
+    }
+    
+     public ArrayList<ImageObject> loadProyect() throws IOException, ClassNotFoundException{
+        File myFile =new File(this.path);
+        ArrayList<ImageObject> objetoList= new ArrayList<ImageObject>();
+        if(myFile.exists()){
+            ObjectInputStream ObjectinputStream=new ObjectInputStream(new FileInputStream(myFile));
+            Object aux= ObjectinputStream.readObject();
+            objetoList=(ArrayList<ImageObject>) aux;
+            ObjectinputStream.close();
+        }//If       
+           
+        return objetoList;
+    }//obteneObjeto
+}
